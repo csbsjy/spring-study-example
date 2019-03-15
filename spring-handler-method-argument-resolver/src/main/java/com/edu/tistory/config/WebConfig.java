76584e3711a763d24a70d6cc38a870fe2606b11e
csbsjy <seojaeyeon@lotte.net>
2019-03-16 01:34:24 +0900

    restore repository

---
+++ spring-handler-method-argument-resolver/src/main/java/com/edu/tistory/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.edu.tistory.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.edu.tistory.custom.LoginUserResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+	
+	@Bean
+	public LoginUserResolver loginUserResolver() {
+		return new LoginUserResolver();
+	}
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(loginUserResolver());
+		WebMvcConfigurer.super.addArgumentResolvers(resolvers);
+	}
+}
