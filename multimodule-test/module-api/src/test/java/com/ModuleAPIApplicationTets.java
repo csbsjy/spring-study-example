1c455f0b7f0eae7d52900fa8401e25eb52715280
csbsjy <seojaeyeon@lotte.net>
2019-06-09 17:28:36 +0900

    multi module 완료

--- multimodule-test/module-api/src/test/java/com/ModuleAPIApplicationTets.java
+++
@@ -1,8 +0,0 @@
-package com;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class ModuleAPIApplicationTets {
-    public void contextLoads() {}
-}
--- multimodule-test/module-web/src/main/java/com/module/web/MemberController.java
+++ multimodule-test/module-web/src/main/java/com/module/web/MemberController.java
@@ -1,8 +1,18 @@
 package com.module.web;
 
+import com.module.common.domain.Member;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 
+@RestController
+@AllArgsConstructor
 public class MemberController {
 
+
+    @GetMapping("/")
+    public String memberInfo(){
+        return new Member("1234","test","1234").toString();
+    }
 
 }
