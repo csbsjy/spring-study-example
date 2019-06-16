90d781b4d121bcf0a68f684fe6b0c413e14d3fbb
csbsjy <seojaeyeon@lotte.net>
2019-06-16 18:17:13 +0900

    package 이동, bean creating error수정

---
+++ multi-module-gradle/module-fo/src/main/java/com/module/ModuleFoApplication.java
@@ -0,0 +1,13 @@
+package com.module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleFoApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ModuleFoApplication.class, args);
+    }
+
+
+}
